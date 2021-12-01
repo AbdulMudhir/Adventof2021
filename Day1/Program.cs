7a04d5c0097909010587bf26e124cba064f33e88
Abdul Mudhir <58626735+AbdulMudhir@users.noreply.github.com>
2021-12-01 21:42:44 +0000

    added day 1 code

---
+++ Day1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Advent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var input =File.ReadAllText("input.txt").Split("\n").Select( x=> int.Parse(x)).ToList();
+                     
+             var total  = 0;
+
+             for(int i = 0; i < input.Count() ; i++)
+             {
+                 if (i == 0)
+                    continue;
+                
+                if(input[i] > input[i-1])
+                    total+=1;
+                    
+             }
+
+             Console.WriteLine(total);
+             
+             total = 0;
+
+              for(int i = 0; i < input.Count() ; i++)
+             {
+                  if (i == 0)
+                    continue;
+                  if (i == input.Count() - 2)
+                    break;
+                if(input[i]+input[i+1]+input[i+2] > input[i-1]+input[i]+input[i+1])
+                    total+=1;
+
+             }
+
+             Console.WriteLine(total);
+
+        }
+    }
+}
